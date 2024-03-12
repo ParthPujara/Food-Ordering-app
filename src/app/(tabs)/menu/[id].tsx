bb035e7d9b2dcca24fd57c6a42c7d52dafe920a5
parthtpujara@gmail.com <ParthtPujara@gmail.com>
2024-03-12 15:34:21 +0530

    Product Detaisl Screen

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,13 +1,89 @@
-import { Stack, useLocalSearchParams } from 'expo-router';
-import React from 'react';
-import { View, Text } from 'react-native';
+import { Stack, useLocalSearchParams } from "expo-router";
+import { View, Text, StyleSheet, Image, Pressable } from "react-native";
+import products from "@assets/data/products";
+import { defaultImage } from "@src/components/ProductListComponent";
+import { useState } from "react";
+import Button from "@src/components/Button";
 
 export default function ProductDetailsScreen() {
-  const {id} = useLocalSearchParams();
+  const { id } = useLocalSearchParams();
+  const product = products.find((item) => item.id.toString() === id);
+  const sizes = ["S", "M", "L", "XL"];
+  const [selectedSize, setSelectedSize] = useState("S");
+  if (!product)
+    return (
+      <View style={styles.container}>
+        <Text>Product not found</Text>
+      </View>
+    );
+
+  const addToCart = () => {
+    console.warn("Add to cart", product.id, selectedSize);
+  }
   return (
-    <View>
-      <Stack.Screen options={{title:"Detials "+id}}/>
-      <Text style={{color: "white"}}>{id}</Text>
-     </View>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name + "AS" }} />
+      <Image
+        source={{ uri: product.image || defaultImage }}
+        style={styles.image}
+      />
+      <Text>Select Size</Text>
+      <View style={styles.sizes}>
+        {sizes.map((size) => (
+          <Pressable
+            key={size}
+            style={[
+              styles.size,
+              {
+                backgroundColor: selectedSize === size ? "gainsboro" : "white",
+              },
+            ]}
+            onPress={() => setSelectedSize(size)}
+          >
+            <Text
+              style={[
+                styles.sizeText,
+                { color: selectedSize === size ? "black" : "gainsboro" },
+              ]}
+            >
+              {size}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
+      <Text style={styles.price}>${product.price}</Text>
+      <Button text="Add to cart" onPress={addToCart} />
+    </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  price: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginTop: "auto",
+  },
+  sizes: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  size: {
+    width: 50,
+    borderRadius: 25,
+    aspectRatio: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  sizeText: {
+    fontSize: 16,
+    fontWeight: "500",
+  },
+});
--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function MenuStach() {
   return (
     <Stack>
-      <Stack.Screen name="menu" options={{ title: "Menu" }}></Stack.Screen>;
+      <Stack.Screen name="menu" options={{ title: "Menu" }}></Stack.Screen>
     </Stack>
   );
 }
